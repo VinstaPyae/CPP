0c18e5ebde83e6ce1ca0d6ea7e36546757019630
Jace <pyaephyozaw16300@gmail.com>
2025-08-05 00:12:59 +0800

    cpp01 ex02 modi

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,10 +6,10 @@ int main(){
     std::string* stringPTR = &brain;
     std::string& stringREF = brain;
     std::cout << "The memory address of the brain variable: " << &brain << std::endl;
-    std::cout << "The memory address of the stringPTR variable: " << stringPTR << std::endl;
-    std::cout << "The memory address of the stringREF variable: " << &stringREF << std::endl;
+    std::cout << "The memory address held by the stringPTR variable: " << stringPTR << std::endl;
+    std::cout << "The memory address held by the stringREF variable: " << &stringREF << std::endl;
     std::cout << "The value of the brain variable: " << brain << std::endl;
     std::cout << "The value pointed to by the stringPTR variable: " << *stringPTR << std::endl;
-    std::cout << "The value of the stringREF variable: " << stringREF << std::endl;
+    std::cout << "The value pointed to by the stringREF variable: " << stringREF << std::endl;
     return 0;
 }
