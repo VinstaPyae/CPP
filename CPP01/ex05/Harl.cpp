800ff46f37059ee61fb2955064e742df2a3da39d
Jace <pyaephyozaw16300@gmail.com>
2025-08-11 20:57:26 +0800

    CPP01 ex05 finished

---
+++ CPP01/ex05/Harl.cpp
@@ -0,0 +1,38 @@
+#include "Harl.hpp"
+
+void Harl::debug() {
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info() {
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you want to pay extra, just let me know!" << std::endl;
+}
+
+void Harl::warning() {
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I've been coming here for years and you just started working here last month." << std::endl;
+}
+
+void Harl::error() {
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level) {
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void (Harl::*funcPtrs[]) () = {
+        &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+    for (int i = 0; i < 4; ++i) {
+        if (level == levels[i]) {
+            (this->*funcPtrs[i])();
+            return;
+        }
+    }
+    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+}
