9a331c78e7905370e37efeab7a1312eccdde2336
Pyae Phyo Zaw <pzaw@c2r1s6.42singapore.sg>
2025-11-29 13:41:43 +0800

    07 00

---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,30 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include "iostream"
+#include "string"
+
+template <typename T> void swap(T &x, T &y)
+{
+    T z = x;
+    x = y;
+    y = z;
+}
+
+template <typename T> const T& max(const T& x, const T& y)
+{
+    if (x > y)
+        return x;
+    else
+        return y;
+}
+
+template <typename T> const T& min(const T& x, const T& y)
+{
+    if (x < y)
+        return x;
+    else
+        return y;
+}
+
+#endif
