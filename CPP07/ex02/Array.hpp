ed9f6b6ed8b67d59263a5459b69c97b568ccf23c
pzaw <pzaw@student.42singapore.sg>
2025-12-01 00:42:04 +0800

    07 02

---
+++ CPP07/ex02/Array.hpp
@@ -0,0 +1,80 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+#include <exception>
+#include <cstdlib>
+
+template <typename T> class Array
+{
+	private:
+		T* _data;
+		unsigned int _size;
+	public:
+		Array() : _data(NULL), _size(0) {}
+
+		Array(unsigned int n) : _data(NULL), _size(n)
+		{
+			if (_size == 0)
+				_data = NULL;
+			else
+				_data = new T[_size]();
+		}
+
+		Array(const Array& other) : _data(NULL), _size(0)
+		{
+			if (other._size > 0)
+			{
+				_size = other._size;
+				_data = new T[_size];
+				for (unsigned int i = 0; i < _size; ++i)
+				{
+					_data[i] = other._data[i];
+				}
+			}
+		}
+
+		Array& operator=(const Array& other)
+		{
+			if (this != &other)
+			{
+				delete[] _data;
+				_size = other._size;
+				if (_size == 0)
+					_data = NULL;
+				else
+				{
+					_data = new T[_size];
+					for (unsigned int i = 0; i < _size; ++i)
+						_data[i] = other._data[i];
+				}
+			}
+			return *this;                                                                      
+		}
+
+		~Array()
+		{
+			delete[] _data;
+		}
+
+		T& operator[](unsigned int index)
+		{
+			if (index >= _size)
+				throw std::out_of_range("Array index out of range\n");
+			return _data[index];
+		}
+
+		const T& operator[](unsigned int index) const
+		{
+			if (index >= _size)
+				throw std::out_of_range("Array index out of range\n");
+			return _data[index];
+		}
+
+		unsigned int size() const
+		{
+			return _size;
+		}
+};
+
+#endif
